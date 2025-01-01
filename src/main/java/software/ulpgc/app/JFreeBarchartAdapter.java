64590640668c00c1e5c913d1a7e8d3ef10aa9fb7
John Doe <juangarciaboissier@gmail.com>
2025-01-01 16:31:19 +0000

    Final Commit

--- src/main/java/software/ulpgc/app/JFreeBarchartAdapter.java
+++ src/main/java/software/ulpgc/app/JFreeBarchartAdapter.java
@@ -2,27 +2,55 @@ package software.ulpgc.app;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 import software.ulpgc.model.Barchart;
 
+import java.awt.*;
+
+import static java.awt.Color.GREEN;
+import static java.awt.Color.RED;
+
 public class JFreeBarchartAdapter {
 
     public static JFreeChart adapt(Barchart barchart) {
-        return ChartFactory.createBarChart(
+        JFreeChart chart = ChartFactory.createBarChart(
                 barchart.getTitle(),
                 barchart.getxAxis(),
                 barchart.getyAxis(),
                 datasetof(barchart)
         );
+        CategoryPlot plot = chart.getCategoryPlot();
+        BarRenderer renderer = (BarRenderer) plot.getRenderer();
 
+        setColorGradient(barchart, renderer, RED, GREEN);
+
+        renderer.setItemMargin(-5);
+
+        return chart;
     }
 
     private static CategoryDataset datasetof(Barchart barchart) {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         for (String line : barchart.keySet()) {
-            dataset.addValue(barchart.get(line),"",line);
+            dataset.addValue(barchart.get(line),line,line);
         }
         return dataset;
+    }
+
+    private static void setColorGradient(Barchart barchart, BarRenderer renderer, Color initial, Color end) {
+        int size = barchart.keySet().size();
+        for (int col = 0; col < size; col++) {
+            double ratio = (double) col / (size-1);
+            int r= (int) (initial.getRed()-ratio*(initial.getRed()- end.getRed()));
+            int g= (int) (initial.getGreen()-ratio*(initial.getGreen()- end.getGreen()));
+            int b= (int) (initial.getBlue()-ratio*(initial.getBlue()- end.getBlue()));
+
+            Color color=new Color(r,g,b);
+            renderer.setSeriesPaint(col, color);
+        }
     }
 }
--- src/main/java/software/ulpgc/app/Main.java
+++ src/main/java/software/ulpgc/app/Main.java
@@ -4,17 +4,33 @@ package software.ulpgc.app;
 import software.ulpgc.control.SelectStatisticCommand;
 import software.ulpgc.io.StatisticBarchartBuilder;
 
+import java.io.File;
 import java.io.IOException;
+
+import static software.ulpgc.io.ResourceDownloader.download;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        GetERWfile();
+
         MainFrame mainFrame = new MainFrame();
         StatisticBarchartBuilder builder = new StatisticBarchartBuilder();
         mainFrame.put("selectStatistic",
-                new SelectStatisticCommand(mainFrame.getDisplay(), builder, mainFrame.getDialog()));
-
+                new SelectStatisticCommand(
+                        mainFrame.getDisplay(),
+                        builder,
+                        mainFrame.getDialog()));
         mainFrame.getDisplay().show(builder.load(0));
 
         mainFrame.setVisible(true);
+    }
+
+    private static void GetERWfile() {
+        File path = new File("");
+        String Path = path.getAbsolutePath();
+        String urlInput= "https://raw.githubusercontent.com/Juanelboi/Resources/refs/heads/main/ERW.csv";
+        String urlOutputPath = Path+"\\ERW.csv";
+        download(urlInput, urlOutputPath);
     }
 }
