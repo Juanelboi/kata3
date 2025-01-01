bfd4efd94411630c5b6968f1e09216718073d4a4
John Doe <juangarciaboissier@gmail.com>
2025-01-01 15:25:06 +0000

    first commit

--- src/main/java/software/ulpgc/app/MainFrame.java
+++ src/main/java/software/ulpgc/app/MainFrame.java
@@ -1,15 +1,7 @@
 package software.ulpgc.app;
 
-import software.ulpgc.control.Command;
-import software.ulpgc.view.BarchartDisplay;
-import software.ulpgc.view.SelectStatisticDialog;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainFrame extends JFrame {
 
